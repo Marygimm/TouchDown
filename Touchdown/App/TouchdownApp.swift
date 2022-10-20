856f7b2e25dc918e37f897a4ba5fc1f221542e8b
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-20 21:55:36 +0000

    Start project touchdown add assets and folders

---
+++ Touchdown/App/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Mary Moreira on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Mary Moreira on 20/10/2022.
+//
+
+import SwiftUI
