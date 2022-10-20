693374674825a45f9b69bf18e5661179ff0c5766
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-20 22:04:47 +0000

    create footerview

--- Touchdown/Utility/Constant.swift
+++ Touchdown/Utility/Constant.swift
@@ -6,3 +6,16 @@
 //
 
 import SwiftUI
+
+//DATA
+
+//COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray)
+
+//LAYOUT
+//UX
+//API
+//IMAGE
+//FONT
+//STRING
